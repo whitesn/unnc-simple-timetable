813a6de17f460211a4a13ea63d7272e981181fb1
whitesn <dylan@whitesn.net>
2015-09-14 11:15:52 +0800

    Fix Invalid Date for Teaching Week.

--- define.php
+++ define.php
@@ -1,7 +1,7 @@
 <?php
 define('SITE_TITLE', 'UNNC Timetabling v2.0');
 define('TIMETABLE_CACHE_DIR', 'timetables-cache/');
-define('BASE_TIME_FALL_SEMESTER', '09-15');
+define('BASE_TIME_FALL_SEMESTER', '09-14');
 define('TOTAL_WEEKS_FALL_SEMESTER', '17');
 define('BALANCE_LOW', '15');
 define('BALANCE_CRIT', '5');
