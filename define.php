5c20306cddc40904437a56a58a559a47933b4222
whitesn <dylan@whitesn.net>
2015-10-14 09:36:38 +0800

    Revert to 813a6de17f460211a4a13ea63d7272e981181fb1

--- define.php
+++ define.php
@@ -2,7 +2,6 @@
 define('SITE_TITLE', 'UNNC Timetabling v2.0');
 define('TIMETABLE_CACHE_DIR', 'timetables-cache/');
 define('BASE_TIME_FALL_SEMESTER', '09-14');
-define('ACADEMIC_WEEK_ADDITION', '3');
 define('TOTAL_WEEKS_FALL_SEMESTER', '17');
 define('BALANCE_LOW', '15');
 define('BALANCE_CRIT', '5');
